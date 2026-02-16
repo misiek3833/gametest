bef0412b92ef04429d9a5630edbb08867a6ac652
misiek3833 <46085811+misiek3833@users.noreply.github.com>
2026-02-16 18:39:31 +0100

    Update firebaseConfig.js

--- ORAVIA/src/firebaseConfig.js
+++ ORAVIA/src/firebaseConfig.js
@@ -2,16 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
-apiKey: "AIzaSyB20wycPxrJIBAzKxBPlzszODznKyHjVbs",
+const firebaseConfig = {
+  apiKey: "AIzaSyB20wycPxrJIBAzKxBPlzszODznKyHjVbs",
   authDomain: "test-af758.firebaseapp.com",
   projectId: "test-af758",
   storageBucket: "test-af758.firebasestorage.app",
   messagingSenderId: "477097409635",
-  appId: "1:477097409635:web:acd2b43c2bb836fc17cc56"
+  appId: "1:477097409635:web:acd2b43c2bb836fc17cc56",
+  databaseURL: "https://test-af758-default-rtdb.europe-west1.firebasedatabase.app"
 };
 
 // Inicjalizacja
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+export const database = getDatabase(app);
 export const auth = getAuth(app);
 export default app;
